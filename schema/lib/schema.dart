9727a4a7840d5f7b1dfabea68ac8d12e990308b6
t3kt <tommy.etkin@gmail.com>
2016-11-29 17:33:34 -0800

    adding JSON serialization

--- schema/lib/schema.dart
+++ schema/lib/schema.dart
@@ -1,7 +1,5 @@
 library tctrl.schema;
 
-import 'package:dartson/dartson.dart';
-
 enum ParamType {
   unknown,
   other,
@@ -25,19 +23,34 @@ ParamType parseParamType(String str) {
       orElse: () => ParamType.other);
 }
 
-class ParamOption {
+abstract class _JsonWritable {
+  Map<String, Object> get jsonDict;
+}
+
+class ParamOption implements _JsonWritable {
   final String key;
   final String label;
 
   ParamOption(this.key, {this.label});
+
+  @override
+  Map<String, Object> get jsonDict => {'key': this.key, 'label': this.label};
 }
 
-abstract class SpecNode {
+abstract class SpecNode implements _JsonWritable {
   final String key;
   String label;
   List<String> tags;
 
   SpecNode(this.key, {this.label, this.tags});
+
+  @override
+  Map<String, Object> get jsonDict =>
+      {
+        'key': this.key,
+        'label': this.label,
+        'tags': this.tags,
+      };
 }
 
 abstract class ParamSpec extends SpecNode {
@@ -48,6 +61,15 @@ abstract class ParamSpec extends SpecNode {
   ParamType get type;
 
   ParamSpec(String key) : super(key);
+
+  @override
+  Map<String, Object> get jsonDict =>
+      _merge(super.jsonDict, second: {
+        'length': this.length == 1 ? null : this.length,
+        'style': this.style,
+        'group': this.group,
+        'type': this.type.toString(),
+      });
 }
 
 class _RangedParamSpec<T> extends ParamSpec {
@@ -61,11 +83,20 @@ class _RangedParamSpec<T> extends ParamSpec {
   final ParamType type;
 
   _RangedParamSpec(String key, this.type)
-  : super(key);
+      : super(key);
+
+  @override
+  Map<String, Object> get jsonDict =>
+      _merge(super.jsonDict, second: {
+        'minLimit':this.minLimit,
+        'maxLimit':this.maxLimit,
+        'minNorm': this.minNorm,
+        'maxNorm': this.maxNorm,
+        'default': this.defaultValue,
+      });
 }
 
 class _MenuParamSpec extends ParamSpec {
-  String group;
   String defaultValue;
   List<ParamOption> options;
 
@@ -73,14 +104,31 @@ class _MenuParamSpec extends ParamSpec {
   ParamType get type => ParamType.menu;
 
   _MenuParamSpec(String key) : super(key);
+
+  @override
+  Map<String, Object> get jsonDict =>
+      _merge(super.jsonDict, second: {
+        'default': this.defaultValue,
+        'options': this.options?.map((o) => o.jsonDict),
+      });
 }
 
 class ModuleSpec extends SpecNode {
   String moduleType;
+  String group;
   List<ParamSpec> params;
   List<ModuleSpec> children;
 
   ModuleSpec(String key) : super(key);
+
+  @override
+  Map<String, Object> get jsonDict =>
+      _merge(super.jsonDict, second: {
+        'moduleType': this.moduleType,
+        'group': this.group,
+        'params': this.params?.map((p) => p.jsonDict),
+        'children':this.children?.map((p) => p.jsonDict),
+      });
 }
 
 class AppSchema extends SpecNode {
@@ -88,4 +136,44 @@ class AppSchema extends SpecNode {
   List<ModuleSpec> children;
 
   AppSchema(String key) : super(key);
+
+  @override
+  Map<String, Object> get jsonDict =>
+      _merge(super.jsonDict, second: {
+        'description': this.description,
+        'children': this.children?.map((p) => p.jsonDict),
+      });
+}
+
+Map<String, Object> _cleanJsonObject(Map<String, Object> obj) {
+  var result = new Map<String, Object>();
+  obj.forEach((key, val) {
+    if (val == null || val == '') {
+      return;
+    }
+    if (val is List && (val as List).isEmpty) {
+      return;
+    }
+    if (val is Map) {
+      result[key] = _cleanJsonObject(val);
+    } else {
+      result[key] = val;
+    }
+  });
+  return result;
+}
+
+Map<String, Object> _merge(Map<String, Object> first,
+    {Map<String, Object> second,
+    List<Map<String, Object>> more}) {
+  var result = new Map<String, Object>.from(first);
+  if (second != null) {
+    result.addAll(second);
+  }
+  for (var map in more) {
+    if (map != null) {
+      result.addAll(map);
+    }
+  }
+  return result;
 }
